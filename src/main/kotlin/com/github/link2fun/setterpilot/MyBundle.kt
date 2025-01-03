f72f96401d7f8f40d9b78ac98993aaea671094af
GitHub Action <action@github.com>
2025-01-03 05:09:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/link2fun/setterpilot/MyBundle.kt
+++ src/main/kotlin/com/github/link2fun/setterpilot/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.link2fun.setterpilot
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/link2fun/setterpilot/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/link2fun/setterpilot/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.link2fun.setterpilot.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
